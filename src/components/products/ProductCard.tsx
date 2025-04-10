59db131375afa6edbf3b4e7c55566b1975684a68
PKirill-106 <p.kirill.design@gmail.com>
2025-04-11 00:59:34 +0200

    Make fully responsive product component & product list component

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -23,30 +23,32 @@ export default function ProductCard({ product }: ProductCardProps) {
 		: `/${category?.slug}/${product.slug}`
 
 	return (
-		<div className='flex flex-col bg-white max-w-90 aspect-[2/3] overflow-hidden rounded-lg shadow-sm hover:scale-105 hover:shadow-lg transition-all duration-300'>
-			<Image
-				src={product.images[0]}
-				alt={product.name}
-				width={267}
-				height={267}
-				className='w-full h-[66%] object-cover mb-3'
-			/>
-			<div className='flex flex-col h-[34%] p-4 pt-0 justify-between'>
-				<div>
+		<div className='flex flex-col bg-white max-w-90 overflow-hidden rounded-lg shadow-sm hover:scale-105 hover:shadow-lg transition-all duration-300'>
+			<div className=' mb-1'>
+				<Image
+					src={product.images[0]}
+					alt={product.name}
+					width={267}
+					height={267}
+					className='w-full h-full aspect-square object-cover'
+				/>
+			</div>
+			<div className='flex flex-col flex-1 p-2 md:p-2 lg:p-3 pt-0 justify-between'>
+				<div className='h-15 md:h-17 lg:h-18 xl:h-21'>
 					<p className='caption mb-1'>{category?.name || ''}</p>
 					<Link href={productUrl}>
-						<h3 className='line-clamp-2 '>{product.name}</h3>
+						<h3 className='line-clamp-2'>{product.name}</h3>
 					</Link>
 				</div>
 
-				<div className='flex justify-between items-center pt-0 lg:pt-6 xl:pt-8'>
+				<div className='flex justify-between items-center pt-2 md:pt-3 lg:pt-4 xl:pt-6'>
 					<p className='price '>{product.price} грн</p>
 					<div className='flex gap-3 lg:gap:4 xl:gap-5'>
 						<button className='hover:text-red-600 transition'>
-							<Heart className='h-6 w-6 md:h-8 md:w-8 lg:h-9 lg:w-9 xl:h-10 xl:w-10' />
+							<Heart className='h-6 w-6 md:h-7 md:w-7 lg:h-8 lg:w-8' />
 						</button>
-						<button className='hover:text-accent transition duration-200'>
-							<ShoppingBag className='h-6 w-6 md:h-8 md:w-8 lg:h-9 lg:w-9 xl:h-10 xl:w-10 text-accent hover:text-button-text transition duration-200' />
+						<button className='text-accent hover:text-button-text transition duration-200'>
+							<ShoppingBag className='h-6 w-6 md:h-7 md:w-7 lg:h-8 lg:w-8' />
 						</button>
 					</div>
 				</div>
