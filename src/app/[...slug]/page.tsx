3ebe299d2a315c033f2a25a066547405a2f911e5
PKirill-106 <p.kirill.design@gmail.com>
2025-05-01 13:29:04 +0200

    fix: filter products correctly by category and subcategory

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -4,24 +4,50 @@ import { getAllProducts } from '@/lib/services/productServices'
 import { ICategory, IProduct } from '@/types/Interfaces'
 import ProductFilters from '../filters/ProductFilters'
 import PaginationControls from '../PaginationControls'
+import { slugify } from 'transliteration'
 
 export default async function CategoryPage({
 	params,
 }: {
-	params: { slug: string[] }
+	params: Promise<{ slug?: string[] }>
 }) {
 	const products: IProduct[] = await getAllProducts()
 	const categories: ICategory[] = await getAllCategories()
 
-	
+	const { slug = [] } = await params
+	const categorySlug = slug[1] ?? null
+	const subcategorySlug = slug[2] ?? null
+
+	const activeCategory = categories.find(
+		cat => slugify(cat.name) === categorySlug && !cat.parentCategoryId
+	)
+	const activeSubcategory = categories.find(
+		cat => slugify(cat.name) === subcategorySlug && cat.parentCategoryId
+	)
+	let filteredProducts = products
+
+	if (activeSubcategory) {
+		filteredProducts = products.filter(
+			p => p.categoryId === activeSubcategory.id
+		)
+	} else if (activeCategory) {
+		const subcategoryIds = categories
+			.filter(cat => cat.parentCategoryId === activeCategory.id)
+			.map(cat => cat.id)
+		filteredProducts = products.filter(p =>
+			subcategoryIds.includes(p.categoryId)
+		)
+	}
 
 	return (
 		<div className='container'>
-			<h2 className='mb-6'>Каталог</h2>
+			<h2 className='mb-6'>
+				{activeSubcategory?.name || activeCategory?.name || 'Каталог'}
+			</h2>
 
 			<ProductFilters categories={categories} />
 
-			<ProductGrid categories={categories} products={products} />
+			<ProductGrid categories={categories} products={filteredProducts} />
 
 			<PaginationControls />
 		</div>
