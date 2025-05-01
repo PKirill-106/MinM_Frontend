2eb4c6322abe61e5082fe8accb5de571c9b47fc8
PKirill-106 <p.kirill.design@gmail.com>
2025-05-01 13:30:06 +0200

    feat: add fallback message when no products are found in selected category

--- src/components/products/ProductGrid.tsx
+++ src/components/products/ProductGrid.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 import ProductCard from './ProductCard'
 
 export default function ProductGrid({ products, categories }: IProductGrid) {
+	if (products.length === 0) {
+		return <h3 className='flex justify-center my-30'>Відсутні продукти у вибраній категорії</h3>
+	}
+
 	return (
 		<div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-5 lg:gap-6'>
 			{products.map(product => (
